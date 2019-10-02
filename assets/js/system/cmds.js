ee988cd7f57ff0c168d1c73b502d0d0df0b2929c
Coder the Great <m_dehghan9112@outlook.com>
2019-10-02 23:09:36 +0330

    Module for Handlers of SYSCALLS

---
+++ assets/js/system/cmds.js
@@ -0,0 +1,63 @@
+permissionDenied = function (cmdComponents) {
+    //
+};
+
+invalidCommand = function (cmdComponents) {
+    //
+};
+
+cd = function (path) {
+    invalidCommand(cd);
+};
+
+mv = function (from, to) {
+    invalidCommand(mv);
+};
+
+rmdir = function (path) {
+    invalidCommand(rmdir);
+};
+
+rm = function (file) {
+    invalidCommand(rm);
+};
+
+touch = function () {
+    invalidCommand(touch);
+};
+
+ls = function () {
+    //
+};
+
+cat = function (cmdComponents) {
+    //
+};
+
+whoami = function (cmdComponents) {
+    //
+};
+
+sudo = function () {
+    //
+};
+
+date = function (cmdComponents) {
+    //
+};
+
+help = function () {
+    //
+};
+
+clear = function () {
+    //
+};
+
+reboot = function () {
+    //
+};
+
+reset = function () {
+    //
+};
